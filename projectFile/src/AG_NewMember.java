1133185538f6e667b1be55237ffba27d53c1499f
squirrelyCoding <georgeabbie0@gmail.com>
2024-10-08 09:40:47 -0500

    merged pull request to main for the GUI

---
+++ projectFile/src/AG_NewMember.java
@@ -0,0 +1,16 @@
+public class AG_NewMember {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String dateHired;
+    private String hardSkills;
+    private int memberID;
+    
+    //constructor for new member
+    public AG_NewMember(String firstName, String lastName, String email, String dateHired){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+}
