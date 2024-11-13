447460ef5e5a76c2d113ad74dfd6e9c09bd09a01
squirrelyCoding <georgeabbie0@gmail.com>
2024-11-13 08:40:43 -0600

    AG: Moved SK Project into main src and removed some test comments to make things look neater

--- projectFile/src/MyFrame.java
+++ projectFile/src/MyFrame.java
@@ -9,25 +9,18 @@ import javax.swing.JOptionPane;
 
 public class MyFrame extends JFrame implements ActionListener{ // Implements the method "MyFrame" so it can be used in main.
 
-    // JButton TestButton;          //*****This globaly identifies "TestButton" as a JButton so that the public method "actionPerformed" can annalyze when the button is pressed
-    // JTextField TestText;         //*****This globaly identified "TestText" as a JTextField so that the public method "actionPerformed" can annalyze the contents of the TextBox
     
     private JTextField nameField, dateHiredField, skillsField;
     private JButton saveButton, editButton;
     private newMember currentEmployee;
     MyFrame() {
-        // TestButton = new JButton("Display Text");            //*****Declares the new JButton labeled "TestButton" (The button will also display text that says "Display Text")
-        // TestText = new JTextField("Display Text");           //*****Declares the new JTextField labeled "TestText" (The textbox will also display text that says "Display Text")
-
+        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // Sets the JFrame so that the window will, by default, close when the x is clicked in the top right corner
         this.setLayout(null);           // Set's the layout of the JFrame as "null" (or none) since I'm not sure which layout we want to use yet (We can discuss this at one of our meetings on an upcoming Thursday)
         this.setSize(500,600);      // Set's the JFrame's default size. This is very changable, but I thought this is a moderate size for a small app that's easy to use.
 
         this.getContentPane().setBackground(new Color(200, 111, 247));
 
-        // TestButton.setBounds(50, 375, 400, 50);          //*****Set's the dimensions of the button. It's displayed at 50 x, and 375 y (50 to the right, and 375 down) and it's size is 400x50 (400 wide, 50 height)
-        // TestButton.addActionListener(this);          //*****Add's a function to the button that allows for the method "actionPerformed" to react accourdingly to when the button is pressed.
-        // this.add(TestButton);            //*****Add's "TestButton" to the UI.
 
         nameField = new JTextField(20);
         dateHiredField = new JTextField(20);
@@ -59,9 +52,6 @@ public class MyFrame extends JFrame implements ActionListener{ // Implements the
         this.add(saveButton);
         this.add(editButton);
         
-        // TestText.setBounds(150,200,200,100);         //*****Set's the dimensions of the textbox. 
-        // TestText.setEditable(true);          //*****Allows for the user to edit the text in "TestText" until it's set to false.
-        // this.add(TestText);          //*****Add's "TestText" to the UI.
 
         this.setVisible(true); // Set's the JFrame and all variables listed above as Visible upon startup.
     }
@@ -74,12 +64,6 @@ public class MyFrame extends JFrame implements ActionListener{ // Implements the
             editEmployee();
         }
 
-
-        // if(e.getSource()==TestButton) {          //*****Scans for when "TestButton" is pressed.
-        //     TestButton.setVisible(false);            //*****Makes "TestButton" invisible and un-pressable.
-        //     System.out.println(TestText.getText());          //*****Print's whatever is written in "TestText" to the terminal.
-        //     TestText.setEditable(false);         //*****Disallows users from editing the text within "TestText" until it's set to true again (Which can't happen in this example).
-        // }
     }
 
         private void saveEmployee() {
