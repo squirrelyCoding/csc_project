5e29903ae264201553cad7a0c8f4b3431607e3e8
Sravani-droid <skadiyala2@murraystate.edu>
2024-12-02 00:21:30 -0600

    changes

---
+++ projectFile/EmployeeManagementSystem.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Employee Management System");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+
+            JTabbedPane tabbedPane = new JTabbedPane();
+            tabbedPane.addTab("Employee List", new EmployeeListPanel());
+            tabbedPane.addTab("Job History", new JobHistoryPanel());
+            tabbedPane.addTab("Job Types", new JobTypePanel());
+            tabbedPane.addTab("Employee Profile", new EmployeeProfilePanel());
+
+            frame.add(tabbedPane);
+            frame.setVisible(true);
+        });
+    }
+}
