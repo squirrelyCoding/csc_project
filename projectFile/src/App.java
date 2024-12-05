784e1fad0067f81f95d243c606fed4a10bad700d
JacksonCash <jackson.w.cash04@gmail.com>
2024-12-05 04:44:09 -0600

    DB Integration 1.2

--- projectFile/src/App.java
+++ projectFile/src/App.java
@@ -69,38 +69,56 @@ public class App {
         }
         return answer;
     }
-    public static void saveEmployee(String name, String dateHired, String skills) throws SQLException {
+    public static void saveInfo(String name, String age, String email, String phone, String HS1, String HS2, String HS3, String SS1, String SS2, String SS3, String date) throws SQLException {
         boolean taken = false;
-        String queryStr = "SELECT * FROM employees";
+        String jdbcEmployeeUrl = "jdbc:sqlite:Employees.db";
+        String jdbcEmpEXPUrl = "jdbc:sqlite:EmpEXP.db";
+        String jdbcSprintUrl = "jdbc:sqlite:SprintEval.db";
+        String employeeStr = "SELECT * FROM employees";
+        String empEXPStr = "SELECT * FROM empEXP";
+        String sprintStr = "SELECT * FROM sprintEval";
+        String sprintQuery = "INSERT INTO sprintEval(id";
         String rowCount = "SELECT COUNT(*) FROM employees";
-        String jdbcUrl = "jdbc:sqlite:Employees.db";
-        Connection conn = DriverManager.getConnection(jdbcUrl);
+        Connection conn = DriverManager.getConnection(jdbcEmployeeUrl);
         Statement statement = conn.createStatement();
         ResultSet Result = statement.executeQuery(rowCount);
+        ResultSetMetaData RSMD;
         Random rand = new Random();
         int IssueID = 1 + rand.nextInt(10);
+        int tempInt;
         Result.next();
         if (!(Result.getInt(1) >= 10)) {
-            Result = statement.executeQuery(queryStr);
+            Result = statement.executeQuery(employeeStr);
             while (Result.next()) {
                 while (Result.getInt("id") == IssueID) {
                     IssueID = 1 + rand.nextInt(10);
-                    Result = statement.executeQuery(queryStr);
+                    Result = statement.executeQuery(employeeStr);
                 }
             }
-            statement.executeUpdate("INSERT INTO employees(id, name, age) VALUES(" + IssueID + ", '" + name + "', " + (19 + rand.nextInt(42)) + ");");
+            // Inserts info into first DB
+            statement.executeUpdate("INSERT INTO employees(id, name, age, email, phoneNum, dateHired)" +
+            " VALUES(" + IssueID + ", '" + name + "', '" + age + "', '" + email + "', '" + phone + "', '" + date + "');");
+            // Inserts info into second DB
+            conn = DriverManager.getConnection(jdbcEmpEXPUrl);
+            statement = conn.createStatement();
+            statement.executeUpdate("INSERT INTO empEXP(id, hardSkill1, hardSkill2, hardSkill3, softSkill1, softSkill2, softSkill3)" + 
+            " VALUES(" + IssueID + ", '" + HS1 + "', '" + HS2 + "', '" + HS3 + "', '" + SS1 + "', '" + SS2 + "', '" + SS3 + "');");
+            // Inserts info into third DB
+            conn = DriverManager.getConnection(jdbcSprintUrl);
+            statement = conn.createStatement();
+            statement.executeUpdate("INSERT INTO sprintEval(id) VALUES(" + IssueID + ");");
         } else {
             System.out.println("DB Full");
         }
     }
-    public static void editEmployee(int ID, String Name, int Age) throws SQLException {
+    public static void editInfo(int ID, String Name, int Age) throws SQLException {
         String jdbcUrl = "jdbc:sqlite:Employees.db";
         Connection conn = DriverManager.getConnection(jdbcUrl);
         Statement statement = conn.createStatement();
         statement.executeUpdate("UPDATE employees SET name=" + Name + " WHERE id=" + ID);
         statement.executeUpdate("UPDATE employees SET age=" + Age + " WHERE id=" + ID);
     }
-    public static void deleteEmployee(int ID) throws SQLException {
+    public static void deleteInfo(int ID) throws SQLException {
         String jdbcUrl = "jdbc:sqlite:Employees.db";
         Connection conn = DriverManager.getConnection(jdbcUrl);
         Statement statement = conn.createStatement();
@@ -110,3 +128,17 @@ public class App {
         new MyFrame();
     }
 }
+
+// Note to self "!!USE FOR DB3 QUERY'S!!"
+//             Result = statement.executeQuery(sprintStr);
+//             RSMD = Result.getMetaData();
+//             for(tempInt = 1; tempInt < RSMD.getColumnCount(); ++tempInt) {
+//                 sprintQuery = (sprintQuery + ", '" + tempInt);
+//             }
+//             Result = statement.executeQuery(sprintStr);
+//             RSMD = Result.getMetaData();
+//             sprintQuery = sprintQuery + "') VALUES(" + IssueID;
+//             for(tempInt = 1; tempInt < RSMD.getColumnCount(); ++tempInt) {
+//                 sprintQuery = (sprintQuery + ", NULL");
+//             }
+//             sprintQuery = (sprintQuery + ");");
